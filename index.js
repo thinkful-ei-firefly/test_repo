0a654e4544b18febc6f116e2a890aff94a60563f
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-08 15:33:30 -0400

    finished rock paper scissors

--- index.js
+++ index.js
@@ -40,31 +40,71 @@ const decode = message => {
 console.log(decode("craft block argon meter bells brown croon droop"));
 
 function daysInMonth(month, leapYear) {
-  switch(month) {
-    case 'January': 
+  switch (month) {
+    case "January":
       return `${month} has 31 days`;
-    case 'Feburary': 
+    case "Feburary":
       return leapYear ? `${month} has 29 days` : `${month}'has 28 days`;
-    case 'March':
+    case "March":
       return `${month} has 31 days`;
-    case 'April':
+    case "April":
       return `${month} has 30 days`;
-    case 'May':
+    case "May":
       return `${month} has 31 days`;
-    case 'June':
+    case "June":
       return `${month} has 30 days`;
-    case 'July':
+    case "July":
       return `${month} has 31 days`;
-    case 'August':
+    case "August":
       return `${month} has 31 days`;
-    case 'September':
+    case "September":
       return `${month} has 30 days`;
-    case 'October':
+    case "October":
       return `${month} has 31 days`;
-    case 'November':
+    case "November":
       return `${month} has 30 days`;
-    case 'December':
+    case "December":
       return `${month} has 31 days`;
   }
 }
-console.log(daysInMonth('May', true))
+console.log(daysInMonth("May", true));
+
+const compChoice = () => {
+  const compNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(compNumber);
+  switch (compNumber) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+    default:
+      return "server error";
+  }
+};
+const rockPaperScissors = num => {
+  if (num === 1) {
+    const userChoice = "rock";
+    const compSelect = compChoice();
+    if (compSelect === "scissors") return "User wins";
+    else if (compSelect === "paper") return "User loses";
+    else return "it's a draw";
+  } else if (num === 2) {
+    const userChoice = "paper";
+    const compSelect = compChoice();
+    if (compSelect === "scissors") return "User loses";
+    else if (compSelect === "paper") return "it's a draw";
+    else return "User Wins!";
+  } else if (num === 3) {
+    const userChoice = "scissors";
+    const compSelect = compChoice();
+    if (compSelect === "scissors") return "It's a draw";
+    else if (compSelect === "paper") return "User Wins";
+    else return "User Loses";
+  } else {
+    return "please enter either a 1 a 2 or a 3";
+  }
+};
+
+console.log(rockPaperScissors(3));
