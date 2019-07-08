a2567638f9bb4d1562b2b8f5ada24ff6400b34d0
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-08 14:57:18 -0400

    decode function solved

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-
 function jediName(firstName, lastName) {
   lName = lastName.slice(0, 3);
   fName = firstName.slice(0, 2);
   return lName + fName;
 }
 
-console.log(jediName('bob', 'smith'));
+console.log(jediName("bob", "smith"));
 
 function beyond(num) {
   if (num === Infinity) {
-    console.log('And beyond.');
-  } else if (num.isFinite() && > 0) {
-    console.log('To infinity');
-  } else if (num.isFinite() && < 0) {
-    console.log('To negative infinity');
-  } else (num === 0) {
-    console.log('Staying home');
+    console.log("And beyond.");
+  } else if (num.isFinite() && num > 0) {
+    console.log("To infinity");
+  } else if (num.isFinite() && num < 0) {
+    console.log("To negative infinity");
+  } else {
+    console.log("Staying home");
   }
-  }
+}
+
+const decode = message => {
+  const arr = message.split(" ");
+
+  const answer = arr.map(word => {
+    if (word[0] === "a") {
+      return word[2];
+    } else if (word[0] === "b") {
+      return word[3];
+    } else if (word[0] === "c") {
+      return word[4];
+    } else if (word[0] === "d") {
+      return word[5];
+    } else {
+      return " ";
+    }
+  });
+  return answer.join(" ");
+};
+
+console.log(decode("craft block argon meter bells brown croon droop"));
