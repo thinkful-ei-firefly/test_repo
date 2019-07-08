18bd47ac40350d8bcc679f6602830fa5c9b3ea70
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-08 17:35:14 -0400

    fix

--- index.js
+++ index.js
@@ -132,3 +132,21 @@ function average(num) {
 }
 const arr = [1, 2, 3, 4, 5];
 console.log(average(arr));
+
+const fizzBuzz = countTo => {
+  const arr = [];
+  for (let i = 1; i <= countTo; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("fizzbuzz");
+    } else if (i % 5 === 0) {
+      arr.push("fizz");
+    } else if (i % 3 === 0) {
+      arr.push("buzz");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
+console.log(fizzBuzz(29));
